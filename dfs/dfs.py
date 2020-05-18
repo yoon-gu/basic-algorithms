040e90827cbf08522acdca69bb995112e8660f97
Yoonku Hwang <yz0624@gmail.com>
2020-05-19 00:49:27 +0900

    Update dfs.py

--- dfs/dfs.py
+++ dfs/dfs.py
@@ -37,15 +37,18 @@ g.add_edge(5, 4);
 g.add_edge(6, 4);
 
 
-here = 0
+here = 2
 g.stack.append(here)
 
 while len(g.stack) > 0 :
 	here = g.stack.pop()
-	print("{0}-\t{0} is removed from the stack.".format(here))
-	g.visit[here] = True
+	if not g.visit[here]:
+		print("{0}-\t{0} is removed from the stack.".format(here))
+		g.visit[here] = True
 	for i in range(n):
-		if (g.adj[here][i] != 0) and (not g.visit[i]) and (not g.stack_history[i]):
+		if (g.adj[here][i] != 0) and (not g.visit[i]):
 			g.stack.append(i)
 			print("\t{} is added to the stack.".format(i))
 			g.stack_history[i] = True
+
+	print(g.stack)
